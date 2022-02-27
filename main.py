0298d6629d80e4ee3543c9dde8de43ef22fdf14a
JoelFox19 <jjfox200529@gmail.com>
2022-02-28 10:46:22 +1300

    test

--- main.py
+++ main.py
@@ -1,2 +1,18 @@
-import flask
-print('hi')
+#This code was created by Joel Fox in 2021
+from flask import Flask, render_template, abort
+import sqlite3
+
+
+app = Flask(__name__)
+
+
+
+#This query takes all of the images and names from the brands table and displays it on the home page(home.html)
+@app.route('/')
+def home():
+   return render_template('home.html',title="Home Page")
+
+
+if __name__=="__main__":
+   app.run(debug=True)
+   
